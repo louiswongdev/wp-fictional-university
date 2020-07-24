a492110e5c995cd5f7cb9a1eaf9696f8a3187283
Louis <louis@Louiss-Mac-mini.local>
2020-07-24 16:09:51 -0400

    Added CPT as must use plugin and Custom Field for our date field of Event post type

--- functions.php
+++ functions.php
@@ -16,7 +16,6 @@
   add_action('wp_enqueue_scripts', 'university_files');
 
 
-
   function university_features() {
     register_nav_menu( 'headerMenuLocation', 'Header Menu Location' );
     register_nav_menu( 'footerLocationOne', 'Footer Location One' );
@@ -24,3 +23,8 @@
     add_theme_support( 'title-tag' );
   }
   add_action( 'after_setup_theme', 'university_features' );
+
+
+
+  
+
