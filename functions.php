1cc3fb7a5c697ec9da2ee8ecac1691d69196f585
Louis <louis@Louiss-Mac-mini.local>
2020-08-02 20:00:45 -0400

    Created campus CPT. Integrated google maps with campus posts

--- functions.php
+++ functions.php
@@ -37,12 +37,14 @@
     wp_enqueue_style('custom-google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCD6EGSZQbDa5wBQYAjnoOSP_6h1YWamDs', NULL, '1.0', true);
+
     if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
       wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     } else {
       wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-      wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.361ae884830d52b8ba98.js'), NULL, '1.0', true);
-      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.361ae884830d52b8ba98.css'));
+      wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.78d8a807efb60902c5d8.js'), NULL, '1.0', true);
+      wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.78d8a807efb60902c5d8.css'));
     }
     
   }
@@ -62,6 +64,10 @@
   add_action( 'after_setup_theme', 'university_features' );
 
   function university_adjust_queries($query) {
+    if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+      $query->set('posts_per_page', -1);
+    } 
+    
     if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
       $query->set('orderby', 'title');
       $query->set('order', 'ASC');
@@ -85,7 +91,8 @@
   }
   add_action( 'pre_get_posts', 'university_adjust_queries' );
 
-
-
-  
-
+  function universityMapKey($api) {
+    $api['key'] = 'AIzaSyCD6EGSZQbDa5wBQYAjnoOSP_6h1YWamDs';
+    return $api;
+  }
+  add_filter('acf/fields/google_map/api', 'universityMapKey');
