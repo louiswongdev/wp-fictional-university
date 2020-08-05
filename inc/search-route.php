10bd5207f45cb43c7357a8ef334838e5de5e0ecf
Louis <louis@Louiss-Mac-mini.local>
2020-08-05 16:03:06 -0400

    Improve search with search-route.php with establishing relationshop between program and campuses/events

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,7 +2,7 @@
 
   function universityRegisterSearch() {
     register_rest_route('university/v1', 'search', array(
-      'methods' => WP_REST_SERVER::READABLE, // 'Get'
+      'methods' => WP_REST_SERVER::READABLE, // same as 'GET'
       'callback' => 'universitySearchResults',
     ));
   }
@@ -29,32 +29,116 @@
         array_push($results['generalInfo'], array(
           'title' => get_the_title(),
           'url' => get_the_permalink(),
+          'postType' => get_post_type(),
+          'authorName' => get_the_author(),
         ));
       }
+
       if (get_post_type() == 'professor') {
         array_push($results['professors'], array(
           'title' => get_the_title(),
           'url' => get_the_permalink(),
+          'image' => get_the_post_thumbnail_url(0, 'professorLandscape') // 0 --> current post
         ));
       }
+
       if (get_post_type() == 'program') {
+        $relatedCampuses = get_field('related_campus');
+        
+        if ($relatedCampuses) {
+          foreach($relatedCampuses as $campus) {
+            array_push($results['campuses'], array(
+              'title' => get_the_title($campus), // specific title we want to get is from campus, not program
+              'url' => get_the_permalink($campus),
+            ));
+          }
+        }
+
         array_push($results['programs'], array(
           'title' => get_the_title(),
           'url' => get_the_permalink(),
+          'id' => get_the_id(),
         ));
       }
+
       if (get_post_type() == 'campus') {
         array_push($results['campuses'], array(
           'title' => get_the_title(),
           'url' => get_the_permalink(),
         ));
       }
+      
       if (get_post_type() == 'event') {
+        $eventDate = new DateTime(get_field('event_date'));
+        $description = null;
+
+        if (has_excerpt()) {
+          $description = get_the_excerpt();
+        } else {
+          $description = wp_trim_words(get_the_content(), 18);
+        }
+
         array_push($results['events'], array(
           'title' => get_the_title(),
           'url' => get_the_permalink(),
+          'month' => $eventDate->format('M'),
+          'day' => $eventDate->format('d'),
+          'description' => $description,
         ));
       }
+    }
+
+    if ($results['programs']) {
+      $programsMetaQuery = array('relation' => 'OR');
+  
+      foreach($results['programs'] as $item) {
+        array_push($programsMetaQuery, array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+      }
+  
+      // query to establish relationship between programs and other CPTS (i.e profs) when searching
+      $programRelationshipQuery = new WP_Query(array(
+        'post_type' => array('professor', 'event'),
+        'meta_query' => $programsMetaQuery,
+      ));
+  
+      while($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+
+        if (get_post_type() == 'event') {
+          $eventDate = new DateTime(get_field('event_date'));
+          $description = null;
+  
+          if (has_excerpt()) {
+            $description = get_the_excerpt();
+          } else {
+            $description = wp_trim_words(get_the_content(), 18);
+          }
+  
+          array_push($results['events'], array(
+            'title' => get_the_title(),
+            'url' => get_the_permalink(),
+            'month' => $eventDate->format('M'),
+            'day' => $eventDate->format('d'),
+            'description' => $description,
+          ));
+        }
+
+        if (get_post_type() == 'professor') {
+          array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'url' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape') // 0 --> current post
+          ));
+        }
+      }
+  
+      // remove duplicates
+      $results['professors'] = array_unique($results['professors'], SORT_REGULAR);
+      $results['events'] = array_unique($results['events'], SORT_REGULAR);
     }
 
     return $results;
